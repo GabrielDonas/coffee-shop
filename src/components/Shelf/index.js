4829a41d7fc3f8c8ba4422d44424ace6bc18b0b2
Gabriel Donas <osradecky@gmail.com>
2020-10-22 13:37:56 +0100

    fetch data to the shelf.

--- src/components/Shelf/index.js
+++ src/components/Shelf/index.js
@@ -1,33 +1,25 @@
 import React from 'react'
 import CoffeePlaceholder from '../../assets/coffee-placeholder_1.jpg'
+import data from '../../data'
 import './style.scss'
 
 const Shelf = () => {
     return (
         <div>
             <div className="shelf-container">
-                <div className="shelf-item">
-                    <div className="image-container">
-                        <div style={{backgroundImage: `url(${CoffeePlaceholder})`}}
-                            className="thumbnail">
+                {data.products.map(product => 
+                    <div className="shelf-item">
+                        <div className="image-container">
+                            <div style={{backgroundImage: `url(${CoffeePlaceholder})`}}
+                                className="thumbnail">
+                            </div>
                         </div>
-                    </div>
-                    <h3 className="title">Red Brick <span role="img" aria-label="brazilian flag">🇧🇷</span></h3>
-                    <div className="description">Seasonal blend is a mix of Los Ancestros from Guatemala and Puente Tarrazu from Costa Rica.</div>
-                    <div className="price">£10.00</div>
-                    <button>Add to Cart</button>
+                        <h3 className="title">{product.title} <span role="img" aria-label="brazilian flag">🇧🇷</span></h3>
+                        <div className="description">{product.description}</div>
+                        <div className="price">{product.price}£</div>
+                        <button>Add to Cart</button>
                 </div> 
-                <div className="shelf-item">
-                    <div className="image-container">
-                        <div style={{backgroundImage: `url(${CoffeePlaceholder})`}}
-                            className="thumbnail">
-                        </div>
-                    </div>
-                    <h3 className="title">Red Brick <span role="img" aria-label="guatemala flag">🇬🇹</span></h3>
-                    <div className="description">Seasonal blend is a mix of Los Ancestros from Guatemala and Puente Tarrazu from Costa Rica.</div>
-                    <div className="price">£10.00</div>
-                    <button>Add to Cart</button>
-                </div> 
+                )}
             </div>
         </div>
     )
