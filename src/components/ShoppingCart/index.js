f13725f5f13f7a85af5e4f0a129f71dd17da81e5
Gabriel Donas <osradecky@gmail.com>
2020-10-27 21:52:30 +0000

    🛒 Test reactburger-menu

--- src/components/ShoppingCart/index.js
+++ src/components/ShoppingCart/index.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import { slide as Menu } from 'react-burger-menu'
 
 const ShoppingCart = () => {
+    const showSettings =  e => {
+        e.preventDefault();
+    }
+
     return (
         <div>
-
+            <Menu rigth>
+                <a id="home" className="menu-item" href="/">Home</a>
+                <a id="about" className="menu-item" href="/">About</a>
+                <a id="contact" className="menu-item" href="/">Contact</a>
+                <a onClick={ showSettings } className="menu-item--small" href="">Settings</a>
+            </Menu>
         </div>
     )
 }
