bf4309f0701c54dec26c48f102fd4ef8d69c59c9
Gabriel Donas <osradecky@gmail.com>
2020-10-27 21:39:43 +0000

    🛒 create checkoy menu

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './default.scss'
 import Header from './components/Header/index'
 import Shelf from './components/Shelf/index'
+import ShoppingCart from './components/ShoppingCart/index'
 
 function App() {
   return (
