6c1e478accebb92d5883d8df45e834aeefdf5da5
Gabriel Donas <osradecky@gmail.com>
2020-10-28 19:43:47 +0000

    🔧Adjust Shopping Cart layout

--- src/components/ShoppingCartItems/index.js
+++ src/components/ShoppingCartItems/index.js
@@ -1,10 +1,26 @@
 import React from 'react'
 import './style.scss'
+import CoffeeBag from '../../assets/CoffeeBag.jpg'
 
 const ShoppingCartItems = () => {
     return (
-        <div>
+        <div className="items-container">
+            <div className="item-info">
+                <div className="thumbnail-container">
+                    <div className="thumbnail-cart" style={{backgroundImage: `url(${CoffeeBag})`}}>
+                    </div>
+                </div>
+                <div className="item-description">
+                    <div className="title-cart">RED BRICK</div>
+                    <div className="item-price">£13</div>
+                </div>
+            </div>
             
+            <div className="buttons-container">
+                <button className="remove-item">-</button>
+                <button className="quantity-cart">1</button>
+                <button className="add-item">+</button>
+            </div>
         </div>
     )
 }
