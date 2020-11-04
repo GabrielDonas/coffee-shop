2f9e909828c9ed9cf0af6494bf25be88cfa7f460
Gabriel Donas <osradecky@gmail.com>
2020-11-04 21:14:53 +0000

    Add checkout feature

--- src/components/Checkout/index.js
+++ src/components/Checkout/index.js
@@ -1,12 +1,26 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { ShoppingContext } from '../ShoppingContext/ShoopingContext'
 import './style.scss'
 
 const Checkout = () => {
+    const [, cartProducts] = useContext(ShoppingContext)
+
+    
+    const calculateTotal = () => {
+        let total = 0;
+        if(cartProducts.length !== 0){
+            const result = cartProducts.map(product => product.price * product.quantity).reduce((a, b) => a + b)
+            total = result;
+            }
+            return total
+        }
+        
+
     return (
         <div className="checkout-container">
             <div className="total">
                 <span>Total:</span>
-                <span>£10</span>
+                <span>£{calculateTotal()}</span>
             </div>
         <button>CHECKOUT</button>
         </div>
